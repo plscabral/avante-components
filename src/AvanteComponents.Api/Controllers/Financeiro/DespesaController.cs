b8a3062f99de882a18d64b242c7545025863b54d
Paulo Cabral <paulo.luiz127@gmail.com>
2022-03-28 16:27:37 -0300

    .

---
+++ src/AvanteComponents.Api/Controllers/Financeiro/DespesaController.cs
@@ -0,0 +1,38 @@
+﻿using AvanteComponents.Api.Models.Financeiro;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+
+namespace AvanteComponents.Api.Controllers.Financeiro
+{
+    [ApiController]
+    [Route("api/financeiro/despesa")]
+    public class DespesaController : ControllerBase
+    {
+        [HttpPost("salvar/documento")]
+        public IActionResult SalvarDocumentos([FromForm] DocumentoDespesaModel model)
+        {
+            string path = @"D:\_ged\Modulo\Financeiro\Despesa\";
+
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                using (FileStream fileStream = System.IO.File.Create(path + model.File.FileName))
+                {
+                    model.File.CopyTo(fileStream);
+                    fileStream.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return StatusCode(201);
+        }
+    }
+}
---
+++ src/AvanteComponents.Api/Models/Financeiro/DespesaModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace AvanteComponents.Api.Models.Financeiro
+{
+    public class DespesaModel
+    {
+    }
+
+    public class DocumentoDespesaModel
+    {
+        public string IdDespesa { get; set; }
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Tamanho { get; set; }
+        public string Caminho { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
